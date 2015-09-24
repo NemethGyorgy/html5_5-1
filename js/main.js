dfa014976b332877cceb28fffcb3ab6504c6f6ac
unknown <html@2-00.itfactory.neta>
2015-09-24 19:47:48 +0200

    Teljes anyag kész

--- js/main.js
+++ js/main.js
@@ -5,31 +5,57 @@ var audioHandler = function( div ) {
     this.div = div;
     this.playBtn = div.querySelector( ".play" );
     this.muteBtn = div.querySelector( ".mute" );
+    this.fullScreenBtn = div.querySelector( ".full-screen" );
     this.audio = div.querySelector( "audio" );
+    this.video = div.querySelector( "video" );
     
     // Constructor.
     this.construct = function() {
         
+        // Ha nincs audio elemünk, akkor videót kezelünk, és fordítva.
+        this.mediaElement = this.audio == null ? this.video : this.audio;
+        if ( this.mediaElement == null ) {
+            console.error( "Nem található média elem!" );
+            return;
+        }
+        
         // Lejátszó gomb.
+        var self = this;
         this.playBtn.addEventListener( "click", function() {
-            this.parentNode.handler.togglePlay();
+            self.div.handler.togglePlay();
         }, false );
         
         // Pause gomb.
         this.muteBtn.addEventListener( "click", function() {
-            this.parentNode.handler.toggleMute();
+            self.div.handler.toggleMute();
         }, false );
+        
+        // Ha a full-screen gomb létezik, beállítjuk.
+        if ( this.fullScreenBtn !== null ) {
+            this.fullScreenBtn.addEventListener( "click", function() {
+                
+                if ( self.video.parentNode.classList.contains("col-md-12") ) {
+                    self.video.parentNode.classList.remove( "col-md-12" );
+                    self.video.parentNode.classList.add( "col-md-3" );  
+                } else {
+                    self.video.parentNode.classList.remove( "col-md-3" );
+                    self.video.parentNode.classList.add( "col-md-12" );                                        
+                }
+                
+                
+            }, false );
+        }
         
     };
     
     // Lejátszás.
     this.play = function() {
-        this.audio.play();
+        this.mediaElement.play();
     };
     
     // Lejátszás váltása.
     this.togglePlay = function() {
-        if ( this.audio.paused ) {
+        if ( this.mediaElement.paused ) {
             this.play();
             this.div.classList.add( "played" );
         } else {
@@ -40,18 +66,18 @@ var audioHandler = function( div ) {
     
     // Megállítás.
     this.pause = function() {
-        this.audio.pause();
+        this.mediaElement.pause();
     };
     
     // Némítás.
     this.mute = function() {
-        this.audio.muted = true;
+        this.mediaElement.muted = true;
     };
     
     // Némítás váltása.
     this.toggleMute = function() {
-        this.audio.muted = !this.audio.muted;
-        if ( this.audio.muted ) {
+        this.mediaElement.muted = !this.mediaElement.muted;
+        if ( this.mediaElement.muted ) {
             this.div.classList.add( "muted" );
         } else {
             this.div.classList.remove( "muted" );            
@@ -62,7 +88,7 @@ var audioHandler = function( div ) {
     this.muteAll = function() {
       
         // Összes audio elem kiválasztása.
-        var allAudio = document.querySelectorAll( "audio" );
+        var allAudio = document.querySelectorAll( "audio, video" );
         
         // Egyenként végigjárjuk az összes elemet és elnémítjuk őket.
         Array.prototype.forEach.call( allAudio, function( item ) {
@@ -82,10 +108,10 @@ var audioHandler = function( div ) {
         
         // Megvizsgáljuk, hogy az audió fájl hosszánál ne legyen nagyobb a cTime.
         // Ha nagyobb, akkor a fájl végére lépünk.
-        cTime = cTime < this.audio.duration ? cTime : this.audio.duration;
+        cTime = cTime < this.mediaElement.duration ? cTime : this.mediaElement.duration;
         
         // A pillanatnyi lejátszási hely beállítása.
-        this.audio.currentTime = cTime;
+        this.mediaElement.currentTime = cTime;
         
     };
     
@@ -93,7 +119,7 @@ var audioHandler = function( div ) {
     
 };
 
-var audioDivs = document.querySelectorAll( ".audio-handler-div" );
+var audioDivs = document.querySelectorAll( ".media-handler-div" );
 Array.prototype.forEach.call( audioDivs, function( item ) {
     item.handler = new audioHandler( item );
 } );
