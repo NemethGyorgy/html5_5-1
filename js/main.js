0f8e153ecb944cb286b668737e1b0798d5f27315
unknown <html@2-00.itfactory.neta>
2015-09-24 18:07:52 +0200

    léptetés elkészítése

--- js/main.js
+++ js/main.js
@@ -24,7 +24,48 @@ var audioHandler = function( audio_selector ) {
         this.audio.muted = !this.audio.muted;
     };
     
+    // Összes némítása.
+    this.muteAll = function() {
+      
+        // Összes audio elem kiválasztása.
+        var allAudio = document.querySelectorAll( "audio" );
+        
+        // Egyenként végigjárjuk az összes elemet és elnémítjuk őket.
+        Array.prototype.forEach.call( allAudio, function( item ) {
+            item.muted = true;    
+        } );
+        
+    };
+    
+    // Audió léptetése.
+    this.setTime = function( cTime ) {
+        
+        cTime = parseInt( cTime, 10 );
+        if ( isNaN(cTime) ) {
+            console.error( "A megadott értéknek számnak kell lennie!" );
+            return;
+        }
+        
+        // Megvizsgáljuk, hogy az audió fájl hosszánál ne legyen nagyobb a cTime.
+        // Ha nagyobb, akkor a fájl végére lépünk.
+        cTime = cTime < this.audio.duration ? cTime : this.audio.duration;
+        
+        // A pillanatnyi lejátszási hely beállítása.
+        this.audio.currentTime = cTime;
+        
+    };
+    
 };
 
 var audio1 = new audioHandler( ".audio1" );
 var audio2 = new audioHandler( ".audio2" );
+
+
+
+
+
+
+
+
+
+
